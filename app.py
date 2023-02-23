0e0b6e8bf3fbd06e6fb8ac90162de0dd7bc31133
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-02-23 12:43:57 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_smorest import Api
+
+from db import db
+
+import os
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+
+# highlight-start
+def create_app(db_url=None):
+    # highlight-end
+    app = Flask(__name__)
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    # highlight-start
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    # highlight-end
+    api = Api(app)
+
+    # highlight-start
+    with app.app_context():
+        db.create_all()
+    # highlight-end
+
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+
+    return app
